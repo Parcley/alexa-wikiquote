d9f354d2e4592fb572e09cdab608829be3046c59
Martin Perzl <martin.perzl@web.de>
2017-10-22 21:19:31 +0200

    add some wikiQuote methods and html helpers

--- src/wikiquote.spec.ts
+++ src/wikiquote.spec.ts
@@ -1,4 +1,4 @@
-import {Wikiquote} from "./wikiquote"
+import {Wikiquote, Section, SectionIndexes, Quotes} from "./wikiquote"
 
 describe("Wikiquote", function() {
   
@@ -11,5 +11,40 @@ describe("Wikiquote", function() {
             }
         );
     });
+
+    it("getRandomId returns id of article", function(done) {
+        Wikiquote.getRandomId().subscribe(
+            (id: number) => {
+                expect(id).toBeGreaterThan(0);
+                done();
+            }
+        );
+    });
+
+    it("getSections returns sections of article", function(done) {
+        const id: number = 11810;
+        Wikiquote.getSectionIndexes(id).subscribe(
+            (sections: SectionIndexes) => {
+                expect(sections.sections.length).toBeGreaterThanOrEqual(1);
+                let sectionTitle: string = sections.titles;
+                expect(sectionTitle).toBe("Gespenst");
+                done();
+            }
+        );
+    });
+
+    it("getQuotes returns quotes of article", function(done) {
+        const id: number = 11810;
+        Wikiquote.getQuotesForSection(id, "1").subscribe(
+            (quotes: string[]) => {
+                expect(quotes.length).toBeGreaterThan(0);
+                let quote: string = quotes[0];
+                expect(quote).toBeDefined();
+                expect(quote.length).toBeGreaterThan(0);
+                done();
+            }
+        );
+    });
+
 
 });
--- src/wikiquote.ts
+++ src/wikiquote.ts
@@ -1,11 +1,62 @@
 import {Observable} from "rxjs";
 import {RxHR, RxHttpRequestResponse} from "@akanass/rx-http-request";
+import * as cheerio from "cheerio";
+
+export interface Section {
+    anchor: string,
+    byteoffset: number,
+    fromtitle: string,
+    index: string, 
+    level: string,
+    line: string,
+    number: string,
+    tocLevel: number
+}
+
+export interface SectionIndexes {
+    titles: string, 
+    sections: string[]
+}
 
 export class Wikiquote {
 
+    private static endpoint: string = "https://de.wikiquote.org/w/api.php";
+
+    // https://de.wikiquote.org/wiki/Spezial:ApiSandbox#action=parse&format=json&pageid=11810&prop=sections
+
+    public static getRandomId(): Observable<number> {
+        return RxHR.get(Wikiquote.endpoint+"?action=query&list=random&format=json&rnnamespace=0")
+        .map( (data: RxHttpRequestResponse, idx: number) => {
+            let reply = JSON.parse(data.body);
+            return reply.query.random[0].id;
+        });
+    }
+
+    public static getSectionIndexes(pageId: number): Observable<SectionIndexes> {
+        let uri: string = Wikiquote.endpoint+"?action=parse&format=json&pageid="+pageId+"&prop=sections"
+        console.log(uri);
+        return RxHR.get(uri)
+        .map( (data: RxHttpRequestResponse, idx: number) => {
+            let reply = JSON.parse(data.body);
+            let sectionsIn: Section[] = reply.parse.sections;
+            let sectionsOut: any[] =[];
+            for(let section of sectionsIn) {
+                var splitNum = section.number.split('.');
+                if(splitNum.length > 1 && splitNum[0] === "1") {
+                    sectionsOut.push(section.index);
+                }
+            }
+            // Use section 1 if there are no "1.x" sections
+            if(sectionsOut.length === 0) {
+                sectionsOut.push("1");
+            }
+            return { titles: reply.parse.title, sections: sectionsOut };
+        });
+    }
+
     public static getRandomTitle(): Observable<string> {
         console.log("getting random title");
-        return RxHR.get("https://en.wikiquote.org/w/api.php?action=query&list=random&format=json")
+        return RxHR.get("https://de.wikiquote.org/w/api.php?action=query&list=random&format=json&rnnamespace=0")
         .map( (data: RxHttpRequestResponse, idx: number) => {
             console.log("body: "+data.body);
             let reply = JSON.parse(data.body);
@@ -13,4 +64,34 @@ export class Wikiquote {
         });
     }
 
+    private static htmlToText(inputHtml: string): string{
+        const loaded: CheerioStatic = cheerio.load(inputHtml);
+        let parsed: Cheerio = loaded('*');
+        return parsed.text();
+    }
+
+    private static getAllTopLevelLis(input: string): string[]{
+        let output: string[] = [];
+        const cheerioContent: CheerioStatic = cheerio.load(input);
+        let lis: Cheerio = cheerioContent('li:not(li li)');
+        lis.each( (i: number, element: CheerioElement)=>{
+            let content: CheerioStatic = cheerio.load(element.children[0]);
+            let h: string = content.html();
+            output.push(h);
+        } );
+        return output;
+    }
+
+    public static getQuotesForSection(pageId: number, sectionIndex: string): Observable<string[]> {
+        let uri: string = Wikiquote.endpoint+"?action=parse&format=json&pageid="+pageId+"&noimages=&section="+sectionIndex;
+        console.log(uri);
+        return RxHR.get(uri)
+        .map( (data: RxHttpRequestResponse, idx: number) => {
+            let reply = JSON.parse(data.body);
+            let content = reply.parse.text["*"];
+            let lis: string[] = Wikiquote.getAllTopLevelLis(content);
+            let lisText: string[] = lis.map(Wikiquote.htmlToText);
+            return lisText;
+        });
+    }
 }
